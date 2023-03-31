7e751f74a7b88268cdb2428aa313f20fbd2943fd
spindouken <mlc9181209@gmail.com>
2023-03-30 20:55:01 -0500

    change to str.format

--- test_get_count.py
+++ test_get_count.py
@@ -6,13 +6,13 @@ from models.city import City
 all_objects_count = storage.count()
 state_objects_count = storage.count(State)
 
-print(f"All objects: {all_objects_count}")
-print(f"State objects: {state_objects_count}")
+print("All objects: {}".format(all_objects_count))
+print("State objects: {}".format(state_objects_count))
 
 states = storage.all(State)
-print(f"States: {states}")
+print("States: {}".format(states))
 
 first_state_id = list(states.values())[0].id
 city_objects_count = storage.count(City, first_state_id)
 
-print(f"City objects for the first state: {city_objects_count}")
+print("City objects for the first state: {}".format(city_objects_count))
