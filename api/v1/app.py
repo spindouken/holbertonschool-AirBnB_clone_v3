09df1cec16d1b298bc4de3be8df64bdc3f55ba20
spindouken <mlc9181209@gmail.com>
2023-04-01 16:26:35 -0500

    added error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """appy appers"""
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """handles 404 scenario (page not found)"""
+    code = exception.__str__().split()[0]
+    message = {"error": "Not found"}
+    return make_response(message, code)
 
 
 @app.teardown_appcontext
