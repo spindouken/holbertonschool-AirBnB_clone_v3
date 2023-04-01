788fbd2823505c559c1e8aab8927022fa053eca9
spindouken <mlc9181209@gmail.com>
2023-04-01 16:14:22 -0500

    indentation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -91,19 +91,19 @@ class DBStorage:
         return None
 
     def count(self, cls=None, first_state_id=None):
-    """
-    count the number of objects in storage
-    """
-    all_class = classes.values()
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
 
-    if not cls:
-        count = 0
-        for clas in all_class:
-            count += len(models.storage.all(clas).values())
-    else:
-        if first_state_id:
-            count = len(models.storage.all(cls, first_state_id).values())
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
         else:
-            count = len(models.storage.all(cls).values())
+            if first_state_id:
+                count = len(models.storage.all(cls, first_state_id).values())
+            else:
+                count = len(models.storage.all(cls).values())
 
-    return count
+        return count
