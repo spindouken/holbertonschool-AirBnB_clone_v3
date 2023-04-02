9f0001a4ec51ffaa5ec64bf76b2c94edb924ab2b
spindouken <mlc9181209@gmail.com>
2023-04-02 00:01:35 -0500

    actual init revert

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """API Blueprint"""
 from flask import Blueprint
-from .index import *
-from .states import *
-from .cities import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from .index import *
+from .states import *
+from .cities import *
