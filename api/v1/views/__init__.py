6f0f8b22db936e4e1174872f922dba69e77f0c50
spindouken <mlc9181209@gmail.com>
2023-04-02 01:10:51 -0500

    alternative circular import fix attempt

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,9 +5,10 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from .index import *
-from .states import *
-from .cities import *
-from .amenities import *
-from .users import *
-#from .places import *
+
+import api.v1.views.index
+import api.v1.views.states
+import api.v1.views.cities
+import api.v1.views.amenities
+import api.v1.views.users
+import api.v1.views.places
