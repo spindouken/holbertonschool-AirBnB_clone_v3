cf6b23f761cae989491bccdbf688f8be2b41bdda
spindouken <mlc9181209@gmail.com>
2023-04-02 04:00:44 -0500

    another init edit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,5 +11,5 @@ import api.v1.views.states
 import api.v1.views.cities
 import api.v1.views.amenities
 import api.v1.views.users
-#import api.v1.views.places
+from api.v1.views.places import *
 import api.v1.views.places_reviews
