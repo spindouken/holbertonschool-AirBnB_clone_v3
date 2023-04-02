c2b015062e583eb570e93f5de6b076b28f2b1223
spindouken <mlc9181209@gmail.com>
2023-04-02 00:00:15 -0500

    cities attempt, init revert

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 """API Blueprint"""
 from flask import Blueprint
-from functools import wraps
+from .index import *
+from .states import *
+from .cities import *
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-
-def import_deferred(imp):
-    @wraps(imp)
-    def wrapper(*args, **kwargs):
-        return imp(*args, **kwargs)
-    return wrapper
-
-
-@import_deferred
-def import_views():
-    from .index import *
-    from .states import *
-
-
-import_views()
