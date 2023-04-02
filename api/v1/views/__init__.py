2adfb5ab053514c8e656b4d3919dda099004f73b
spindouken <mlc9181209@gmail.com>
2023-04-02 04:33:27 -0500

    last syntax fix attempt

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,10 +6,10 @@ from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
-import api.v1.views.index
-import api.v1.views.states
-import api.v1.views.cities
-import api.v1.views.amenities
-import api.v1.views.places
-import api.v1.views.users
-import api.v1.views.places_reviews
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
