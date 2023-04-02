a4b63eb9699a83924875a89300da3b7abea756d2
spindouken <mlc9181209@gmail.com>
2023-04-01 23:14:55 -0500

    weird compliance issue? + states task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """API Blueprint"""
 from flask import Blueprint
-from .index import *
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from .index import *
+from .states import *
---
+++ api/v1/views/states.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""States API view"""
+from flask import jsonify, request, abort, make_response
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieve all State objects"""
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieve a specific State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Delete a specific State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a new State object"""
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in body:
+        abort(400, 'Missing name')
+    state = State(**body)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Update a specific State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+    for key, value in body.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
